85610012f208a4180769aedbdf2724411873a05c
msmaiaa <roxx77344@gmail.com>
2021-01-29 17:38:13 -0300

    added starting layout, basic steam authentication working

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 export default function Navbar(){
     const handleLogin = () =>{
         const popupWindow = window.open(
-            
-        )
+            process.env.REACT_APP_API_URL + "/auth/steam",
+            "_blank",
+            "width=800, height=600"
+        );
+        if(window.focus) popupWindow.focus();
     }
+
+    useEffect(() =>{
+        window.addEventListener("message", event =>{
+            if(event.origin !== process.env.REACT_APP_API_URL) return;
+
+            const {token, ok, user} = event.data;
+            if(ok){
+                localStorage.setItem("token", token);
+                console.log(user);
+            }
+        })
+    })
 
     return(
         <nav className="nav">
