9bfc29c179edfce0481b6fa376b3e67dd1fcc253
msmaiaa <roxx77344@gmail.com>
2021-01-29 23:13:46 -0300

    all api endpoints working as intended with the mongo db

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -10,15 +10,14 @@ module.exports = {
             return res.status(401).json({message: 'Error with authorization token'})
         }
         const hasUser = await User.findOne({steamid: decoded.steamid});
-        if(hasUser){
-            return res.status(200);
-        }
-        const newUser = User.create({
+        if(hasUser) return res.status(300).json({message:'User already exists'});
+            
+        const newUser = new User({
             steamid: decoded.steamid,
             email: ''
         })
         await newUser.save();
-        return res.status(200);
+        return res.status(200).json({message:'User created successfully'});
     },
     createObservedUser: async(req,res) =>{
         const token = req.get('Authorization');
@@ -26,12 +25,18 @@ module.exports = {
         if(!decoded){
             return res.status(401).json({message: 'Error with authorization token'})
         }
-        const newObsUser = ObservedUser.create({
+        const hasObsUser = await ObservedUser.findOne({observerId: decoded.steamid, steamid: req.body.steamid})
+        if (hasObsUser) return res.status(300).json({message:'Already observing the given steamid'});
+
+        const newObsUser = new ObservedUser({
             observerId: decoded.steamid,
             steamid: req.body.steamid
         })
-        await newUser.save();
-        return res.status(200);
+        newObsUser.save((err,doc)=>{
+            if (err) return res.status(404).json({message:'Error while trying to create a new observed user'})
+            return res.status(200).json({message:'Observed user created successfully'});
+        });
+        
     },
     getObservedUsersList: async (req,res) =>{
         const token = req.get('Authorization');
@@ -39,11 +44,12 @@ module.exports = {
         if(!decoded){
             return res.status(401).json({message: 'Error with authorization token'})
         }
-        ObservedUser.find({steamid: decoded.steamid}, (err,docs)=>{
+        ObservedUser.find({observerId: decoded.steamid}, (err,docs)=>{
             if(err){
                 return res.status(404).json({message: 'Error fetch observed users list'});
             }
-            return res.status(200).json({message: docs});
+            console.log(docs);
+            return res.status(200).json({message: 'Observed users fetched successfully', docs: docs});
         })
     },
     changeEmail: async (req,res) =>{
