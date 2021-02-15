a1b2ca8e9b54c12c39b4554f204c01de671c9ff1
msmaiaa <roxx77344@gmail.com>
2021-02-15 10:40:58 -0300

    add google search meta tag

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App(props) {
       <Helmet>
         <title>Steam-bans</title>
         <meta name="description" content="This is a tool which allows you to track the ban status for any STEAMID and if you wish you can save it to a list. Optionally you can get notified via discord or email when the user gets banned!" />
+        <meta name="google-site-verification" content={process.env.GOOGLE_VERIFICATION} />
       </Helmet>
       <Navbar/>
       <div className="pageContent"> 
